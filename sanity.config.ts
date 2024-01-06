60a720ffff8128dcac44ee692f498e48673609ec
lpdmots <101499709+lpdmots@users.noreply.github.com>
2024-01-06 22:25:17 +0100

    first commit ?

--- sanity.config.ts
+++ sanity.config.ts
@@ -12,8 +12,9 @@ const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
     basePath: "/studio",
-    name: "I_don_t_speak_French",
-    title: "Start French now",
+    name: "default",
+    title: "site-mullheim",
+
     projectId,
     dataset,
 
